18f3cb947536a2d928246d4d1b4f39de715101c5
Illya Boyko <illya.boyko@gmail.com>
2014-11-10 08:36:59 +0100

    ActionBar Sherlock is replaced with ActionBarCompat

--- catroid/src/org/catrobat/catroid/content/bricks/SetVariableBrick.java
+++ catroid/src/org/catrobat/catroid/content/bricks/SetVariableBrick.java
@@ -24,6 +24,7 @@ package org.catrobat.catroid.content.bricks;
 
 import android.content.Context;
 import android.content.res.ColorStateList;
+import android.support.v4.app.FragmentActivity;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
@@ -35,7 +36,6 @@ import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.badlogic.gdx.scenes.scene2d.actions.SequenceAction;
 
 import org.catrobat.catroid.ProjectManager;
@@ -150,8 +150,13 @@ public class SetVariableBrick extends UserVariableBrick {
 						&& ((Spinner) view).getAdapter().getCount() == 1)) {
 					NewDataDialog dialog = new NewDataDialog((Spinner) view, NewDataDialog.DialogType.USER_VARIABLE);
 					dialog.addVariableDialogListener(SetVariableBrick.this);
+<<<<<<< HEAD
 					dialog.show(((SherlockFragmentActivity) view.getContext()).getSupportFragmentManager(),
 							NewDataDialog.DIALOG_FRAGMENT_TAG);
+=======
+					dialog.show(((FragmentActivity) view.getContext()).getSupportFragmentManager(),
+							NewVariableDialog.DIALOG_FRAGMENT_TAG);
+>>>>>>> ActionBar Sherlock is replaced with ActionBarCompat
 					return true;
 				}
 
@@ -164,8 +169,13 @@ public class SetVariableBrick extends UserVariableBrick {
 				if (position == 0 && ((UserVariableAdapterWrapper) parent.getAdapter()).isTouchInDropDownView()) {
 					NewDataDialog dialog = new NewDataDialog((Spinner) parent, NewDataDialog.DialogType.USER_VARIABLE);
 					dialog.addVariableDialogListener(SetVariableBrick.this);
+<<<<<<< HEAD
 					dialog.show(((SherlockFragmentActivity) view.getContext()).getSupportFragmentManager(),
 							NewDataDialog.DIALOG_FRAGMENT_TAG);
+=======
+					dialog.show(((FragmentActivity) view.getContext()).getSupportFragmentManager(),
+							NewVariableDialog.DIALOG_FRAGMENT_TAG);
+>>>>>>> ActionBar Sherlock is replaced with ActionBarCompat
 				}
 				((UserVariableAdapterWrapper) parent.getAdapter()).resetIsTouchInDropDownView();
 				userVariable = (UserVariable) parent.getItemAtPosition(position);
--- catroid/src/org/catrobat/catroid/ui/fragment/FormulaEditorFragment.java
+++ catroid/src/org/catrobat/catroid/ui/fragment/FormulaEditorFragment.java
@@ -32,11 +32,14 @@ import android.content.IntentFilter;
 import android.graphics.Rect;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnKeyListener;
@@ -45,6 +48,7 @@ import android.view.ViewTreeObserver;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 
+<<<<<<< HEAD
 import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.app.SherlockFragment;
 import com.actionbarsherlock.app.SherlockFragmentActivity;
@@ -52,6 +56,8 @@ import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuInflater;
 import com.actionbarsherlock.view.MenuItem;
 
+=======
+>>>>>>> ActionBar Sherlock is replaced with ActionBarCompat
 import org.catrobat.catroid.ProjectManager;
 import org.catrobat.catroid.R;
 import org.catrobat.catroid.content.bricks.Brick;
@@ -69,7 +75,7 @@ import org.catrobat.catroid.ui.dialogs.FormulaEditorComputeDialog;
 import org.catrobat.catroid.ui.dialogs.NewStringDialog;
 import org.catrobat.catroid.utils.ToastUtil;
 
-public class FormulaEditorFragment extends SherlockFragment implements OnKeyListener,
+public class FormulaEditorFragment extends BaseFragment implements OnKeyListener,
 		ViewTreeObserver.OnGlobalLayoutListener {
 	private static final String TAG = FormulaEditorFragment.class.getSimpleName();
 
@@ -119,14 +125,14 @@ public class FormulaEditorFragment extends SherlockFragment implements OnKeyList
 	}
 
 	private void setUpActionBar() {
-		ActionBar actionBar = getSherlockActivity().getSupportActionBar();
+		ActionBar actionBar = getSupportActivity().getSupportActionBar();
 		previousActionBarTitle = ProjectManager.getInstance().getCurrentSprite().getName();
 		actionBar.setDisplayShowTitleEnabled(true);
 		actionBar.setTitle(R.string.formula_editor_title);
 	}
 
 	private void resetActionBar() {
-		ActionBar actionBar = getSherlockActivity().getSupportActionBar();
+		ActionBar actionBar = getSupportActivity().getSupportActionBar();
 		actionBar.setTitle(previousActionBarTitle);
 	}
 
@@ -141,7 +147,12 @@ public class FormulaEditorFragment extends SherlockFragment implements OnKeyList
 
 	public static void showFragment(View view, FormulaBrick formulaBrick, Brick.BrickField brickField) {
 
+<<<<<<< HEAD
 		SherlockFragmentActivity activity = (SherlockFragmentActivity) view.getContext();
+=======
+		FragmentActivity activity = null;
+		activity = (FragmentActivity) view.getContext();
+>>>>>>> ActionBar Sherlock is replaced with ActionBarCompat
 
 		FormulaEditorFragment formulaEditorFragment = (FormulaEditorFragment) activity.getSupportFragmentManager()
 				.findFragmentByTag(FORMULA_EDITOR_FRAGMENT_TAG);
@@ -195,7 +206,7 @@ public class FormulaEditorFragment extends SherlockFragment implements OnKeyList
 		formulaEditorEditText.endEdit();
 		currentFormula.prepareToRemove();
 
-		SherlockFragmentActivity activity = getSherlockActivity();
+		FragmentActivity activity = getSupportActivity();
 		FragmentManager fragmentManager = activity.getSupportFragmentManager();
 		FragmentTransaction fragTransaction = fragmentManager.beginTransaction();
 		fragTransaction.hide(this);
@@ -299,7 +310,7 @@ public class FormulaEditorFragment extends SherlockFragment implements OnKeyList
 							endFormulaEditor();
 							return true;
 						case R.id.formula_editor_keyboard_string:
-							FragmentManager fragmentManager = ((SherlockFragmentActivity) context)
+							FragmentManager fragmentManager = ((FragmentActivity) context)
 									.getSupportFragmentManager();
 							Fragment dialogFragment = fragmentManager
 									.findFragmentByTag(NewStringDialog.DIALOG_FRAGMENT_TAG);
@@ -341,6 +352,7 @@ public class FormulaEditorFragment extends SherlockFragment implements OnKeyList
 			menu.getItem(index).setVisible(false);
 		}
 
+<<<<<<< HEAD
 		MenuItem undo = menu.findItem(R.id.menu_undo);
 		if (!formulaEditorEditText.getHistory().undoIsPossible()) {
 			undo.setIcon(R.drawable.icon_undo_disabled);
@@ -363,6 +375,10 @@ public class FormulaEditorFragment extends SherlockFragment implements OnKeyList
 		menu.findItem(R.id.menu_redo).setVisible(true);
 		getSherlockActivity().getSupportActionBar().setDisplayShowTitleEnabled(true);
 		getSherlockActivity().getSupportActionBar().setTitle(getString(R.string.formula_editor_title));
+=======
+		getSupportActivity().getSupportActionBar().setDisplayShowTitleEnabled(true);
+		getSupportActivity().getSupportActionBar().setTitle(getString(R.string.formula_editor_title));
+>>>>>>> ActionBar Sherlock is replaced with ActionBarCompat
 
 		super.onPrepareOptionsMenu(menu);
 	}
@@ -524,7 +540,7 @@ public class FormulaEditorFragment extends SherlockFragment implements OnKeyList
 	}
 
 	private void showFormulaEditorListFragment(String tag, int actionbarResId) {
-		FragmentManager fragmentManager = ((SherlockFragmentActivity) context).getSupportFragmentManager();
+		FragmentManager fragmentManager = ((FragmentActivity) context).getSupportFragmentManager();
 		Fragment fragment = fragmentManager.findFragmentByTag(tag);
 
 		if (fragment == null) {
@@ -539,6 +555,7 @@ public class FormulaEditorFragment extends SherlockFragment implements OnKeyList
 		((FormulaEditorListFragment) fragment).showFragment(context);
 	}
 
+<<<<<<< HEAD
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
@@ -555,6 +572,10 @@ public class FormulaEditorFragment extends SherlockFragment implements OnKeyList
 
 	private void showFormulaEditorDataFragment(String tag, int actionbarResId) {
 		FragmentManager fragmentManager = ((SherlockFragmentActivity) context).getSupportFragmentManager();
+=======
+	private void showFormulaEditorVariableListFragment(String tag, int actionbarResId) {
+		FragmentManager fragmentManager = ((FragmentActivity) context).getSupportFragmentManager();
+>>>>>>> ActionBar Sherlock is replaced with ActionBarCompat
 		Fragment fragment = fragmentManager.findFragmentByTag(tag);
 
 		if (fragment == null) {
@@ -571,8 +592,13 @@ public class FormulaEditorFragment extends SherlockFragment implements OnKeyList
 			fragment.setArguments(bundle);
 			fragmentManager.beginTransaction().add(R.id.script_fragment_container, fragment, tag).commit();
 		}
+<<<<<<< HEAD
 		((FormulaEditorDataFragment) fragment).setAddButtonListener(getSherlockActivity());
 		((FormulaEditorDataFragment) fragment).showFragment(context);
+=======
+		((FormulaEditorVariableListFragment) fragment).setAddButtonListener(getSupportActivity());
+		((FormulaEditorVariableListFragment) fragment).showFragment(context);
+>>>>>>> ActionBar Sherlock is replaced with ActionBarCompat
 	}
 
 
@@ -634,6 +660,7 @@ public class FormulaEditorFragment extends SherlockFragment implements OnKeyList
 		}
 
 		IntentFilter filterVariableDeleted = new IntentFilter(ScriptActivity.ACTION_VARIABLE_DELETED);
+<<<<<<< HEAD
 		BottomBar.hideBottomBar(getSherlockActivity());
 		filterVariableDeleted.addAction(ScriptActivity.ACTION_USERLIST_DELETED);
 		getActivity().registerReceiver(variableOrUserListDeletedReceiver, filterVariableDeleted);
@@ -644,6 +671,33 @@ public class FormulaEditorFragment extends SherlockFragment implements OnKeyList
 
 		ImageButton backspaceEditText = (ImageButton) getSherlockActivity().findViewById(R.id.formula_editor_edit_field_clear);
 		ImageButton backspaceOnKeyboard = (ImageButton) getSherlockActivity().findViewById(R.id.formula_editor_keyboard_delete);
+=======
+		getActivity().registerReceiver(variableDeletedReceiver, filterVariableDeleted);
+		BottomBar.hideBottomBar(getSupportActivity());
+	}
+
+	public void updateButtonViewOnKeyboard() {
+
+		ImageButton undo = (ImageButton) getSupportActivity().findViewById(R.id.formula_editor_keyboard_undo);
+		if (!formulaEditorEditText.getHistory().undoIsPossible()) {
+			undo.setImageResource(R.drawable.icon_undo_disabled);
+			undo.setEnabled(false);
+		} else {
+			undo.setImageResource(R.drawable.icon_undo);
+			undo.setEnabled(true);
+		}
+
+		ImageButton redo = (ImageButton) getSupportActivity().findViewById(R.id.formula_editor_keyboard_redo);
+		if (!formulaEditorEditText.getHistory().redoIsPossible()) {
+			redo.setImageResource(R.drawable.icon_redo_disabled);
+			redo.setEnabled(false);
+		} else {
+			redo.setImageResource(R.drawable.icon_redo);
+			redo.setEnabled(true);
+		}
+
+		ImageButton backspace = (ImageButton) getSupportActivity().findViewById(R.id.formula_editor_edit_field_clear);
+>>>>>>> ActionBar Sherlock is replaced with ActionBarCompat
 		if (!formulaEditorEditText.isThereSomethingToDelete()) {
 			backspaceEditText.setImageResource(R.drawable.icon_backspace_disabled);
 			backspaceEditText.setEnabled(false);
