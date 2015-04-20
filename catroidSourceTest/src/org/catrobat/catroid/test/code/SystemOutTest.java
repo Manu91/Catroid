d5cb02219715874dbd0b7d565cbad1dbbf9a6659
Manuel Haid <manuel.haid@student.tugraz.at>
2015-04-20 13:26:44 +0200

    bug-fixing

--- catroidSourceTest/src/org/catrobat/catroid/test/code/SystemOutTest.java
+++ catroidSourceTest/src/org/catrobat/catroid/test/code/SystemOutTest.java
@@ -43,7 +43,7 @@ public class SystemOutTest extends TestCase {
 	private static final String[] IGNORED_FILES = { "SystemOutTest.java", "ToastUtil.java", "BTServer.java"};
 	private static final String SYSTEM_OUT = "System.out";
 	private static final String PRINT_STACK_TRACE = ".printStackTrace()";
-	//private static final String TOAST_STRING = "Toast.makeText";
+	private static final String TOAST_STRING = "Toast.makeText";
 	private static final String SUPERTOAST_STRING = "SuperToast";
 
 	private void checkFileForString(File file, String string) throws IOException {
@@ -109,9 +109,9 @@ public class SystemOutTest extends TestCase {
 	}
 
 	public void testForToast() throws IOException {
-		//checkForStringInFiles(TOAST_STRING, STACK_TRACE_DIRECTORIES);
-		//assertFalse("Files with 'Toast.makeText(context, text, duration)' found! \nPlease use 'ToastUtil.showError(context, message), or " +
-		//"ToastUtil.showSuccess(context, message)' instead\n\n" + errorMessages, errorFound);
+		checkForStringInFiles(TOAST_STRING, STACK_TRACE_DIRECTORIES);
+		assertFalse("Files with 'Toast.makeText(context, text, duration)' found! \nPlease use 'ToastUtil.showError(context, message), or " +
+		"ToastUtil.showSuccess(context, message)' instead\n\n" + errorMessages, errorFound);
 	}
 
 	public void testForSuperToast() throws IOException {
--- catroidSourceTest/src/org/catrobat/catroid/test/license/LicenseTest.java
+++ catroidSourceTest/src/org/catrobat/catroid/test/license/LicenseTest.java
@@ -38,8 +38,8 @@ public class LicenseTest extends TestCase {
 	private static final String[] DIRECTORIES = Utils.SOURCE_AND_RESOURCE_DIRECTORIES;
 
 	private List<String> agplLicenseText;
-	public boolean allLicenseTextsPresentAndCorrect;
-	public String errorMessages;
+	private boolean allLicenseTextsPresentAndCorrect;
+	private String errorMessages;
 
 	@Override
 	public void setUp() throws IOException {
@@ -103,8 +103,8 @@ public class LicenseTest extends TestCase {
 	public void testLicensePresentInAllFiles() throws IOException {
 		for (String directoryName : DIRECTORIES) {
 			File directory = new File(directoryName);
-			//assertTrue("Couldn't find directory: " + directoryName, directory.exists() && directory.isDirectory());
-			//assertTrue("Couldn't read directory: " + directoryName, directory.canRead());
+			assertTrue("Couldn't find directory: " + directoryName, directory.exists() && directory.isDirectory());
+			assertTrue("Couldn't read directory: " + directoryName, directory.canRead());
 
 			List<File> filesToCheck = Utils.getFilesFromDirectoryByExtension(directory, new String[] { ".java", ".xml",
 					".rb" });
@@ -112,7 +112,7 @@ public class LicenseTest extends TestCase {
 				checkFileForLicense(file, agplLicenseText);
 			}
 		}
-		//assertTrue("Correct license text was not found in all files:\n" + errorMessages,
-				//allLicenseTextsPresentAndCorrect);
+		assertTrue("Correct license text was not found in all files:\n" + errorMessages,
+				allLicenseTextsPresentAndCorrect);
 	}
 }
